d01fa77a1a2685516e393c9e491ba76924ff509a
BrajsundariRathor <rathorbrajsundari@gmail.com>
2025-01-10 19:07:09 +0530

    add doctors cards html and css file

---
+++ scripts/doctor.js
@@ -0,0 +1,6 @@
+import { navbar } from "./navbar.js";
+import { baseurl } from './baseUrl.js';
+import { footer } from "./footer.js";
+
+navbar();
+footer();
---
+++ scripts/medicines.js
@@ -0,0 +1,7 @@
+
+import { navbar } from "./navbar.js";
+import { baseurl } from './baseUrl.js';
+import { footer } from "./footer.js";
+
+navbar();
+footer();
